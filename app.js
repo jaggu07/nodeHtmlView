e6009f43fb0546cf4db60d0e9f60481254269675
jagan <jagandravid7@gmail.com>
2018-04-04 21:22:59 +0530

    initial

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require("express");
 var swig = require("swig");
 var bodyParser = require("body-parser");
 var cons = require('consolidate');
+var path = require('path');
 
 var app = express();
 app.engine('html', cons.swig)
 app.set('view engine','html');
 app.set('views',__dirname+'/views');
+app.use(express.static(path.join(__dirname)));
 //app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',function(req,res){
